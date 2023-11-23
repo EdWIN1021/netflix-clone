c08885436c0af7aa943f4cbaa7ff1ffd9af3aa50
Edwin <>
2023-11-22 20:54:20 -0800

    fix: clean

--- components/login/LoginModal.tsx
+++ components/login/LoginModal.tsx
@@ -47,16 +47,16 @@ const LoginModal = () => {
 
       <div className="mt-4 flex text-[13px] text-[#b3b3b3]">
         <div className="flex-1 ">
-          <div className="flex items-center">
+          {/* <div className="flex items-center">
             <input
               className="relative mr-1 h-[18px] w-[18px] appearance-none rounded bg-white after:invisible after:absolute after:left-1/2 after:top-1/2 after:h-[10px] after:w-[10px] after:translate-x-[-50%] after:translate-y-[-50%] after:text-center after:leading-[10px]  after:text-black after:content-['✔'] checked:bg-[#8C8C8C] checked:after:visible"
               type="checkbox"
             />
             <span>Remember me</span>
-          </div>
+          </div> */}
         </div>
 
-        <span>Need Help?</span>
+        <Link href={"/"}>Need Help?</Link>
       </div>
 
       <div className="mt-20 text-[#737373]">
@@ -70,9 +70,9 @@ const LoginModal = () => {
         <p className="my-[13px] text-[13px]">
           This page is protected by Google reCAPTCHA to ensure you&apos;re not a
           bot.
-          <a className="text-[#0071eb] hover:underline" href="">
+          <Link className="text-[#0071eb] hover:underline" href="/">
             Learn more.
-          </a>
+          </Link>
         </p>
       </div>
     </form>
