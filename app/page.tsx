9319be35d0bdfce568acf3bb00e23a45cbf78f16
Edwin <>
2023-12-12 18:53:07 -0800

    feat: test

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ import DropDown from "@/components/DropDown";
 export default function Home() {
   return (
     <main className="bg-[rgb(35,35,35,35)]">
-      <BannerSection />
+      {/* <BannerSection />
       <TVSection />
       <DevicesSection />
       <KidSection />
       <AppSection />
-      <QuestionSection />
+      <QuestionSection /> */}
 
       <Footer links={homeFooterLinks} variant="dark" dropDown={<DropDown />} />
     </main>
