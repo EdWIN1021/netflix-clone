d2fbe290e6e4596a0bc71d477c3ab9e745d80104
Edwin <>
2024-01-10 14:21:19 -0800

    feat: test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import AppSection from "../components/AppSection";
+// import AppSection from "../components/AppSection";
 import BannerSection from "../components/BannerSection";
-import DevicesSection from "../components/DevicesSection";
+// import DevicesSection from "../components/DevicesSection";
 import Footer from "../components/Footer";
 // import KidSection from "../components/KidSection";
-import QuestionSection from "../components/QuestionSection";
-import TVSection from "../components/TVSection";
+// import QuestionSection from "../components/QuestionSection";
+// import TVSection from "../components/TVSection";
 import { homeFooterLinks } from "../constants";
 import DropDown from "../ui/DropDown";
 
@@ -12,11 +12,11 @@ const Home = () => {
   return (
     <main className="bg-[rgb(35,35,35,35)]">
       <BannerSection />
-      <TVSection />
+      {/* <TVSection />
       <DevicesSection />
-      {/* <KidSection /> */}
+      <KidSection />
       <AppSection />
-      <QuestionSection />
+      <QuestionSection /> */}
       <Footer
         className="text-[#737373] bg-black"
         links={homeFooterLinks}
